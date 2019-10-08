69451d537fc1929321ad8ce0b34f268193617172
elorafarias <56055785+elorafarias@users.noreply.github.com>
2019-10-08 19:34:56 -0300

    Update aluguel.sol

--- aluguel.sol
+++ aluguel.sol
@@ -1,6 +1,7 @@
 pragma solidity 0.5.12;
 
-contract Locacao {
+contract Locacao 
+{
     
     /*
     para textao 
@@ -10,28 +11,35 @@ contract Locacao {
     string public locador;
     string public locatario;    
     uint256 private valor;
-
-    constructor (string memory nomeLocador,string memory nomeLocatario,uint256 valorDoAluguel)
-                   public {
-            locador = nomeLocador;
-            locatario = nomeLocatario;
-            valor = valorDoAluguel;
-            }
-            function valorDoAluguel () public view returns (uint256) { 
-                return  valor;  
-            }
-                               function simulaMulta (uint256 mesesRestantes,
-                               uint256 totalMesesContrato)
-                               public
-                               view
-                               return (uint256 valorMulta) {
-                                valorMulta = valor "numeroMaximolegaldealuguelparamulta";
-                                   valorMulta = valorMulta/totalMesesContrato;
-                                   valorMulta = valorMulta*mesesRestante;
-                                   return valorMulta; 
-                                                                  }
-                                   
-                                   function reajusteAlugel (uint256 percentualReajuste) public {
-                                       uint256 valorDoAcrescimo = 0;
-                                       }
-}   
+    uint256 constant public numeroMaximolegaldealuguelparamulta = 3;
+    
+    constructor (string memory nomeLocador,string memory nomeLocatario,uint256 valorDoAluguel)public
+     
+    {
+        locador = nomeLocador;
+        locatario = nomeLocatario;
+        valor = valorDoAluguel;
+    }
+    
+    function valorDoAluguel () public view returns (uint256) 
+    { 
+    
+    return  valor;  
+    
+    }
+    
+    function simulaMulta (uint256 mesesRestantes, uint256 totalMesesContrato) public view returns (uint256 valorMulta)
+    {
+        valorMulta = valor*numeroMaximolegaldealuguelparamulta;
+        valorMulta = valorMulta/totalMesesContrato;
+        valorMulta = valorMulta*mesesRestantes;
+        return valorMulta; 
+    }
+    
+    function reajusteAlugel (uint256 percentualReajuste) public 
+    {
+        uint256 valorDoAcrescimo = 0;
+        valorDoAcrescimo = ((valor*percentualReajuste)/100);
+        valor = valor + valorDoAcrescimo;
+    }
+}  
